261ddc7169e09e83eacc38a78c7c013c4ff4c613
mariamorozova01 <mmorozovaa01@gmail.com>
2024-10-14 20:27:38 +0300

    burger menu

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+    function scrollToSchedule() {
+        const scheduleSection = document.getElementById("schedule");
+        scheduleSection.scrollIntoView({ behavior: "smooth" });
+    }
+
+
+    function toggleMenu() {
+        const navOverlay = document.getElementById("nav-overlay");
+        navOverlay.classList.toggle("active");
+    }
+    
